9269a142593bbd38667420fbc64552bb0f853e99
Lev E. Givon <lev@columbia.edu>
2024-07-30 09:38:50 -0400

    Update wheel version (#64).

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ tests_require = ["scikit-learn>=0.24.0", "pytest", "joblib", "black", "click"]
 
 setup(
     name="gap-stat",
-    version="2.0.3",
+    version="2.0.3.1",
     author="Miles Granger",
     maintainer="Miles Granger",
     author_email="miles59923@gmail.com",
